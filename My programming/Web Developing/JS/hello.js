1f2784fc3cd706b3499585dac6e4c01b89e6c3e9
hasanasiabd <hasanasiabd12@gmail.com>
2026-04-15 16:22:11 +0600

    js file

---
+++ My programming/Web Developing/JS/hello.js
@@ -0,0 +1,144 @@
+// var strType = `hasanSir`;
+// console.log(typeof strType);
+
+// var numType = 18;
+// console.log(typeof numType);
+
+// var BoolType = false;
+// console.log(typeof BoolType);
+
+// var rochona = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae debitis deserunt ducimus. Est blanditiis id eius necessitatibus explicabo! Commodi inventore magnam sint libero quibusdam, beatae facere, et qui reiciendis ab, molestias tempore eaque placeat ratione excepturi reprehenderit accusantium. Sint molestias voluptates aut repudiandae consequatur ullam eligendi sunt hic voluptatem non?";
+// console.log(rochona.toUpperCase());
+
+// var goro = 'LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISICING ELIT. REPUDIANDAE DEBITIS DESERUNT DUCIMUS. EST BLANDITIIS ID EIUS NECESSITATIBUS EXPLICABO! COMMODI INVENTORE MAGNAM SINT LIBERO QUIBUSDAM, BEATAE FACERE, ET QUI REICIENDIS AB, MOLESTIAS TEMPORE EAQUE PLACEAT RATIONE EXCEPTURI REPREHENDERIT ACCUSANTIUM. SINT MOLESTIAS VOLUPTATES AUT REPUDIANDAE CONSEQUATUR ULLAM ELIGENDI SUNT HIC VOLUPTATEM NON?';
+// console.log(goro.toLocaleLowerCase());
+
+// var myFd = "Hasan, Jubayer, Ismail, Tahsin, Mazhar";    //split()
+// console.log(myFd.split("Hasan"));
+
+// var myFd = "Hasan Jubayer Ismail Tahsin Mazhar";    //split()
+// console.log(myFd.split("Hasan"));
+
+// var myFd = "Hasan, Jubayer, Ismail, Tahsin, Mazhar";    //indexOf()
+// console.log(myFd.indexOf("M"));
+
+// var num1 = 30;            //+num
+// var num2 = 20;
+// var mot = num1 + num2;
+// console.log(mot);
+
+// var num1 = '30';           //+str
+// var num2 = 20;
+// var mot = num1 + num2;
+// console.log(mot);
+
+// var num1 = '30';           //parseFloat
+// var num2 = 20;
+// num1 = parseFloat(num1)
+// var mot = num1 + num2;
+// console.log(mot);
+
+// var num1 = '30.546514';    //parseInt
+// var num2 = 20;
+// num1 = parseInt(num1)
+// var mot = num1 + num2;
+// console.log(mot);
+
+// var num1 = '30.546514';    //parseFloat 30.654654
+// var num2 = 20;
+// num1 = parseFloat(num1)
+// var mot = num1 + num2;
+// console.log(mot);
+
+// var num3 = 50;             //""+ (str)
+// var num4 =40;
+// num4 = "" + num4;
+// console.log(num4);
+
+// var num3 = 50;           //typeof ""+ (str)
+// var num4 =40;
+// num4 = "" + num4;
+// console.log(typeof num4);
+
+// var num3 = 50;            //""+  total (str)
+// var num4 =40;
+// num4 = "" + num4;
+// var total = num3 + num4;
+// console.log(total);
+
+// var num3 = 50;          //typeof ""+  total (str)
+// var num4 =40;
+// num4 = "" + num4;
+// var total = num3 + num4;
+// console.log(typeof total);
+
+
+           //JavaScript Operators
+
+// var num5 = 20;         // (+)
+// var num6 = 20;
+// var total = num5+num6;
+// console.log(total);
+
+// var num5 = 20;         // (-)
+// var num6 = 30;
+// var total = num5-num6;
+// console.log(total);
+
+// var num5 = 20;         // (-)
+// var num6 = 30;
+// var total = num6-num5;
+// console.log(total);
+
+// var num5 = 20;
+// var num6 = 30;
+// var total = num6*num5;
+// console.log(total);
+
+// var num5 = 20;         // (/)
+// var num6 = 30;
+// var total = num6/num5;
+// console.log(total);
+
+// var num5 = 20;         // (%)
+// var num6 = 30;
+// var total = num6 % num5;
+// console.log(total);
+
+// var num1 = -30;           //Math.abs()
+// var sum = Math.abs(num1);
+// console.log(sum);
+
+
+// var totalMember = 10;    //Math.round()
+
+// var votar = 0.000001;
+// console.log(Math.round(votar));
+
+
+// var totalMember = 10;    //Math.ceil()
+
+// var votar = 0.000001;
+// console.log(Math.ceil(votar));
+
+
+// var totalMember = 10;    //Math.floor()
+
+// var votar = 0.99999;
+// console.log(Math.floor(votar));
+
+
+// var lotary = Math.random();  //Math.random()
+// console.log(lotary);
+
+// var lotary = Math.random() * 100;  //Math.random()
+// console.log(lotary);
+
+
+
+           //Assignment           //Hablu Programmer
+
+
+         //Conditional Statements
+
+
