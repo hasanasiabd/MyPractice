6ffe997241ac334dc165aef2b863784753f31980
hasanasiabd <hasanasiabd12@gmail.com>
2026-05-25 16:03:45 +0600

    cProgramming

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+if(num % 10 == 0){
+        //     printf("You entered %d, Exiting the Loop.\n", num);
+        //     continue; //? This will skip the rest of the loop when i is equal to num, but since it's the last iteration, it will just end the loop.
+        // }
