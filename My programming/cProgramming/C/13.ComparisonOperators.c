8b4dfe7acc77c0d74329c1057426d0798ce265c0
hasanasiabd <hasanasiabd12@gmail.com>
2026-05-29 19:38:30 +0600

    7(20-26) File create & 19(1-19) File update(comment) in C Folder

--- My programming/cProgramming/C/13.ComparisonOperators.c
+++ My programming/cProgramming/C/13.ComparisonOperators.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    int a = 10;
-    int b = 5;
-    int c;
-//* Comparison Operators(>, <, >=, <=)
+// int main() {
+//     int a = 10;
+//     int b = 5;
+//     int c;
+// //* Comparison Operators(>, <, >=, <=)
 
-    c = (a > b);   // Greater than
-    printf("Greater than: %d\n", c);
+//     c = (a > b);   // Greater than
+//     printf("Greater than: %d\n", c);
 
-    c = (a < b);   // Less than
-    printf("Less than: %d\n", c);
+//     c = (a < b);   // Less than
+//     printf("Less than: %d\n", c);
 
-    c = (a >= b);  // Greater than or equal to
-    printf("Greater than or equal to: %d\n", c);
+//     c = (a >= b);  // Greater than or equal to
+//     printf("Greater than or equal to: %d\n", c);
 
-    c = (a <= b);  // Less than or equal to
-    printf("Less than or equal to: %d\n", c);
+//     c = (a <= b);  // Less than or equal to
+//     printf("Less than or equal to: %d\n", c);
 
-    return 0;
-}
+//     return 0;
+// }
