e61c364d1bce701d259ca7f02fa366e8e803ea0a
hasanasiabd <hasanasiabd12@gmail.com>
2026-05-30 12:36:51 +0600

    Edit

---
+++ My programming/cProgramming/C/002.Syntax.c
@@ -0,0 +1,30 @@
+// #include<stdio.h>
+
+// int main() {
+// //?Syntax in C refers to the set of rules that govern how C code should be written and structured. It defines how to write valid C programs, including the use of keywords, operators, data types, and the overall structure of the code.
+// //?Here are some key aspects of C syntax:
+// //?1. Statements: C code is made up of statements, which are instructions that perform specific actions. Each statement must end with a semicolon (;).
+// //?2. Functions: C programs are organized into functions, which are blocks of code that perform specific tasks. The main function is the entry point of a C program, and it must be defined as int main().
+// //?3. Variables: C allows you to declare variables to store data. You must specify the data type of the variable (e.g., int, float, char) and give it a name. For example: int a; float num; char name;.
+// //?4. Data Types: C has several built-in data types, including int (for integers), float (for floating-point numbers), double (for double-precision floating-point numbers), and char (for characters).
+// //?5. Operators: C provides various operators for performing operations on variables and values, such as arithmetic operators (+, -, *, /), relational operators (==, !=, <, >), and logical operators (&&, ||).
+// //?6. Control Structures: C supports control structures like if-else statements, loops (for, while, do-while), and switch statements for controlling the flow of the program.
+
+
+//     int a = 10;
+//     int b = 20;
+//     int num= 144;
+//     float num2 = 334.45;
+//     double num3 = 123.456789;
+//     char name= 'A';
+//     char name2= 'B';
+//     char name3[] = "Homaira Noor"; //! This is a string literal, which is an array of characters terminated by a null character '\0'. The size of the array will be 13 (12 characters + 1 null character).
+    
+//     printf("Hello World!\n");
+//     printf("a+b= %d\n",a+b);
+//     printf("1+2= %d\n", 1+2);
+//     printf("Homaira Noor\n");
+
+//     printf("num: %d\n num2: %f\n num3: %lf\n name: %c\n name2: %c\n name3: %s\n", num, num2, num3, name, name2, name3);
+//     return 0;
+// }
--- My programming/cProgramming/README.md
+++
@@ -1,2 +0,0 @@
-# MyPractice
-MyPractice contains my daily coding practice and experiments. This repository is where I store different practice programs, small projects, and learning materials as I grow my programming skills. Anyone is welcome to explore and learn from the code.
